9ecb41621f2ff00084f36f5d271bcc5d5aa8d936
vkumar786 <vkumar@onemindservices.com>
2024-05-16 19:48:46 +0530

    added more content in about page

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ import streamlit as st
 loaded_model = pickle.load(open("trained_model.sav", "rb"))
 
 # function for prediction
+
+
 def diabetes_prediction(input_data):
     # input_data = (0,137,40,35,168,43.1,2.288,33)
 
@@ -25,7 +27,7 @@ def diabetes_prediction(input_data):
     id_reshaped = id_np_arr.reshape(1, -1)
 
     # Standardized as we have done to model
-    # id_std = scaler.transform(id_reshaped)  
+    # id_std = scaler.transform(id_reshaped)
 
     predict = loaded_model.predict(id_reshaped)
 
@@ -36,13 +38,13 @@ def diabetes_prediction(input_data):
 
 
 def display_main_app():
-    
+
     # Giving a title
     st.title("Diabetes Prediction Web App")
-    
+
     # getting input data from user
- 	# Pregnancies, Glucose, BloodPressure, SkinThickness,	Insulin, BMI, DiabetesPedigreeFunction, Age 	
-    
+    # Pregnancies, Glucose, BloodPressure, SkinThickness,	Insulin, BMI, DiabetesPedigreeFunction, Age
+
     # Dropdown menu for gender selection
     gender = st.selectbox("Select Gender", ["Male", "Female"])
 
@@ -51,12 +53,17 @@ def display_main_app():
     if gender == "Male":
         Pregnancies = 0
     else:
-        Pregnancies = st.number_input("Number of Pregnancies", min_value=0, max_value=1000, step=1)
+        Pregnancies = st.number_input(
+            "Number of Pregnancies", min_value=0, max_value=1000, step=1)
 
-    Glucose = st.number_input("Glucose Level (0 to 200)", min_value=0, max_value=200, step=1)
-    BloodPressure = st.number_input("Blood Pressure Value (0 to 200)", min_value=0, max_value=200, step=1)
-    SkinThickness = st.number_input("Skin Thickness Value (0 to 100)", min_value=0, max_value=100, step=1)
-    Insulin = st.number_input("Insulin Level (0 to 500)", min_value=0, max_value=500, step=1)
+    Glucose = st.number_input(
+        "Glucose Level (0 to 200)", min_value=0, max_value=200, step=1)
+    BloodPressure = st.number_input(
+        "Blood Pressure Value (0 to 200)", min_value=0, max_value=200, step=1)
+    SkinThickness = st.number_input(
+        "Skin Thickness Value (0 to 100)", min_value=0, max_value=100, step=1)
+    Insulin = st.number_input(
+        "Insulin Level (0 to 500)", min_value=0, max_value=500, step=1)
 
     # BMI
     # BMI = st.text_input("BMI Value")
@@ -70,17 +77,19 @@ def display_main_app():
     if weight > 0 and height > 0:
         BMI = weight / (height ** 2)
 
-    DiabetesPedigreeFunction = st.number_input("Diabetes Pedigree Function value (0 to 5)", min_value=0.00, max_value=5.00, step=0.01)
-    Age = st.number_input("Age of Person (0 to 120)", min_value=0, max_value=120, step=1)
-    
-    
+    DiabetesPedigreeFunction = st.number_input(
+        "Diabetes Pedigree Function value (0 to 5)", min_value=0.00, max_value=5.00, step=0.01)
+    Age = st.number_input("Age of Person (0 to 120)",
+                          min_value=0, max_value=120, step=1)
+
     # code for x
     diagnosis = ''
-    
+
     # creating a button for prediction
     if st.button("Diabetes Test Result"):
-        diagnosis = diabetes_prediction([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
-    
+        diagnosis = diabetes_prediction(
+            [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
+
     if diagnosis == "Person is diabetic":
         st.success(diagnosis)
     elif diagnosis == "Person is not diabetic":
@@ -90,7 +99,7 @@ def display_main_app():
 def display_understanding_diabetes_tab():
     # Colorful heading
     st.markdown(
-        "<h1 style='color: #FF4B4B;'>Diabetes Prediction Information</h1>", 
+        "<h1 style='color: #FF4B4B;'>Diabetes Prediction Information</h1>",
         unsafe_allow_html=True
     )
 
@@ -121,7 +130,8 @@ def display_understanding_diabetes_tab():
 
     st.subheader("Age of Person:")
     st.text("The age of the individual in years.")
-    
+
+
 def display_about_tab():
     st.header("Welcome to the Diabetes Prediction Web Application! 👋")
 
@@ -142,15 +152,44 @@ def display_about_tab():
         With its user-friendly interface and powerful predictive capabilities, the Diabetes Prediction Web Application aims to improve healthcare outcomes, enhance patient engagement, and contribute towards a healthier future for all.
         """
     )
-    
+
+    st.divider()
+
+    st.subheader("Data Sources and Methodology")
+
+    st.markdown(
+        """
+        **Data Sources:**
+        The dataset used in this project was obtained from Kaggle. It can be accessed at:
+        """
+    )
+    st.code("https://www.kaggle.com/code/melikedilekci/diabetes-dataset-for-beginners")
+
+    st.markdown(
+        """
+        **Methodology:**
+        The following machine learning algorithms were used in this project:
+        - Support Vector Machine (SVM).
+        - Random Forest.
+        
+        **Algorithm Selection:**
+        - Support Vector Machine (SVM) algorithm was chosen for its robustness in handling complex datasets and its ability to find optimal decision boundaries. SVM performs well in high-dimensional spaces and is effective in cases where the data may not be linearly separable.
+        - Random Forest, on the other hand, was selected for its ensemble learning approach, which combines multiple decision trees to improve prediction accuracy. It is capable of handling large datasets with high dimensionality and is less prone to overfitting compared to individual decision trees.
+        
+        **Technology Used:**
+        This web application was developed using Streamlit, a powerful Python library for creating interactive web applications.
+        """
+    )
+
+
 if __name__ == "__main__":
     st.sidebar.title("Navigation")
     selection = st.sidebar.radio(
         "Go to", ["Main App", "Understanding Diabetes", "About"])
 
     if selection == "Main App":
-        display_main_app()
+        display_about_tab()
     elif selection == "Understanding Diabetes":
         display_understanding_diabetes_tab()
     elif selection == "About":
-        display_about_tab()
+        display_main_app()
